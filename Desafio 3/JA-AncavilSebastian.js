2fe6d361ed73134f8a05cf43a1f41716dd967fd5
spancavil <soancavil@gmail.com>
2021-05-09 13:20:00 -0300

    Optimizado del desafío 3

--- Desafio 3/JA-AncavilSebastian.js
+++ Desafio 3/JA-AncavilSebastian.js
@@ -17,15 +17,21 @@
  * Aclaracion: no usar variables globales y clases y ejecutar con NodeJS (no tsc)
  */
 
-// funcion de espera. Sería SINCRONICO ESTO.
-const esperar = ret => { for (let i = 0; i < ret * 3e6; i++); }
+// funcion de espera. Es una promesa.
+const esperar = ret => {
+    return new Promise ((resolve,reject) => {
+        setTimeout(()=>{
+            resolve(null);
+        },ret)
+    })
+}
 
-// muestra las palabras en orden a partir de un texto
-const mostrarPalabras = (texto, tiempo, cantidadPalabras, callback) => {
+// muestra las palabras en orden a partir de un texto. Hacemos un async await para que espere a cuando se resuelve la promesa.
+const mostrarPalabras = async (texto, tiempo, cantidadPalabras, callback) => {
     let palabras = texto.split(" ");
     for (const palabra of palabras) {
         console.log (palabra);
-        esperar(tiempo);
+        await esperar(tiempo);
     }
     cantidadPalabras += palabras.length; //Sumamos a la cantidad de palabras que había el largo actual;
     console.log (cantidadPalabras);
@@ -35,12 +41,39 @@ const mostrarPalabras = (texto, tiempo, cantidadPalabras, callback) => {
 let texto1 = 'primer texto';
 let texto2 = 'curso backend de coderhouse';
 let texto3 = 'probando llamadas asincronas en nodejs';
-const tiempo = 500;
+const tiempo = 1000;
 
-mostrarPalabras(texto1, tiempo, 0, (totalPalabras, err) => {
-    mostrarPalabras(texto2, tiempo, totalPalabras, (totalPalabras, err) => {
-        mostrarPalabras(texto3, tiempo, totalPalabras, (totalPalabras, err) => {
-            console.log('Proceso terminado, cantidad de palabras: ' + totalPalabras);
-        });
-    });
+//Generamos la entrada del usuario mediante input.
+const inputTime = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout
+  });
+
+//Question sería una especie de prompt, que genera un callback.
+inputTime.question('Ingrese un tiempo en segundos o deje en blanco (default 1 seg):', tiempo => {
+    console.log (tiempo*=1000);
+    tiempo ?  showWords (tiempo): showWords (1000);
+    inputTime.close();
 });
+
+function showWords (time) {
+    mostrarPalabras(texto1, time, 0, (totalPalabras, err) => {
+        mostrarPalabras(texto2, time, totalPalabras, (totalPalabras, err) => {
+            mostrarPalabras(texto3, time, totalPalabras, (totalPalabras, err) => {
+                console.log('Proceso terminado, cantidad de palabras: ' + totalPalabras);
+            });
+        })
+    });
+}
+
+/*Comentario Angie:
+Hola, Sebastian. Muy buen trabajo! 
+
+Solamente te falto hacer configurable el tiempo, es decir, que si a la funcion se le pasa el parametro tiempo, 
+se ejecute con ese tiempo y si no, que se ejecute cada segundo. Seguro conoces el operador ternario que te funciona 
+muy bien para  este caso. Te lo comento por si quieres probarlo:
+
+https://developer.mozilla.org/es/docs/Web/JavaScript/Reference/Operators/Conditional_Operator
+
+Tambien,  como se que eres muy aplicado, trata de realizar este ejercicio usando setInterval
+*/
